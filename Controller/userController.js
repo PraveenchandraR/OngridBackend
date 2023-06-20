512326e8c0549ce1a0a288b9df653ac1b2cf9b3b
Praveen <praveenchandraravuri@gmail.com>
2023-06-20 18:18:02 +0530

    ongridbackend

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,6 +1,7 @@
 const { UserModel,BookModel } = require("../MongoModel/userModel");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { deleteModel } = require("mongoose");
 
 const passHash = async(password)=>{
   try {
@@ -46,95 +47,166 @@ const signup = async (req, res) => {
       password: hashPass,
       phone,
     }).save();
-
-    res.status(200).send({
-      success: true,
-      message: "User SignedUp successfully",
-    });
-  } catch (error) {
-    console.log(error.message);
-    res
-      .status(500)
-      .send({ message: "Some error occurred while performing signup" });
-  }
-};
-
-const login = async(req, res)=>{
-  console.log("login is called..");
-  const {email, password} = req.body;
-  try {
-    if(!email || !password){
-      return res.status(401).send({message: "Please enter complete details to login"})
-    }
-
-    const userFound = await UserModel.findOne({email: email});
-    if(!userFound){
-      return res.status(404).send({message: "User is not registered, signup first"});
-    }
-
-    // console.log("hashed password", userFound.password);
-    const isSamePass = await comparePass(password, userFound.password);
-    if(!isSamePass){
-      return res.status(401).send({message: "Invalid email or password, please try again"});
-    }
-
-    const token = jwt.sign({id: userFound._id}, process.env.JWT_SECRET, {expiresIn: '2d'});
-
-    res.status(200).send({
-      success: true,
-      message: "User loggedIn successfully",
-      user: {
-        name: userFound.name,
-        email: userFound.email,
-        phone:userFound.phone,
-      },
-      token
-    })
-
+ return res.json({status:"ok"})
+    // res.status(200).send({
+    //   success: true,
+    //   message: "User SignedUp successfully",
+    // });
   } catch (error) {
     console.log(error);
-    res.status(500).send({
-      success: false,
-      message: "Some error occurred while logging in",
-      error
-    })
+    return res.status(500).send("Some error occurred while performing signup")
+    // res
+    //   .status(500)
+    //   .send({ message: "Some error occurred while performing signup" });
   }
-  // res.setHeader('Authorization', `Bearer ${token}`);
+};
+const login = async (req,res)=>{
+    const {email,password}=req.body;
+    const user = await UserModel.findOne({email});
+    if(!user){
+        return res.json({status:"error"})
+    }
+    const checkpassword = await bcrypt.compare(password, user.password);
+    if(checkpassword){
+        const token = jwt.sign({email: user.email},process.env.JWT_SECRET, {
+            expiresIn:60,
+        })
+        
+        return res.json({status:"ok", data:token})
+    }else{
+        return res.json({status:"error"})
+    }
+    
 }
 
-const bookDemo = async(req, res) =>{
+// const login = async(req, res)=>{
+//   console.log("login is called..");
+//   const { email, password } = req.body;
+//   console.log(email);
+//   try {
+//     if(!email || !password){
+//       return res.status(401).send({status: "Please enter complete details to login"})
+//     }
 
-  const { name, email, phone, organisation, known } = req.body;
+//     const userFound = await UserModel.findOne({email: email});
+//     if(!userFound){
+//       return res.status(404).send({message: "User is not registered, signup first"});
+//     }
 
-  const userFound = await UserModel.findOne({ email: email });
-  
-  try {
-    if (!userFound) {
-    return res.status(404).send({message: "User is not registered, signup first"});
-  }
-    if(!name || !email || !phone || !organisation || !known){
-      return res.status(401).send("Please enter all the deatils");
+//     // console.log("hashed password", userFound.password);
+//     const isSamePass = await comparePass(password, userFound.password);
+//     if(!isSamePass){
+//       return res.status(401).send({status: "Invalid email or password, please try again"});
+//     }
+
+//     var token = jwt.sign({ id: userFound._id }, process.env.JWT_SECRET, { expiresIn: 20 });
+//     console.log(token);
+
+//     res.status(200).send({
+//       success: true,
+//       status: "User loggedIn successfully",
+//       user: {
+//         name: userFound.name,
+//         email: userFound.email,
+//         phone:userFound.phone,
+//       },
+//       token
+//     })
+
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send({
+//       success: false,
+//       status: "Some error occurred while logging in",
+//       error
+//     })
+//   }
+//   // res.setHeader('Authorization', `Bearer ${token}`);
+// }
+
+
+const bookDemo = async (req,res)=>{
+    const {name,email,phone,organisation,known} = req.body;
+    const user = await UserModel.findOne({email});
+    if(!user){
+       return res.json("This email is not registered. if you not signup to OnGrid you can't book a demo please signup")
     }
-    const demoUser = await new BookModel({
-      name,
-      email,
-      phone,
-      organisation,
-      known
-    }).save(); 
-             res.status(200).send({
-           success: true,
-            message: "User Demobooking successful",
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({message: "Something went wrong"});
-  }
-  res.send({message: "Protected route"});
+    try{
+        const bookdemoobj = new BookModel({
+            name,
+            email,
+            phone,
+            organisation,
+            known
+        })
+        const bookinsert = await bookdemoobj.save();
+        
+        return res.status(200).json("demo booked").send(bookinsert)
+    }
+    catch(err){
+        console.log(err)
+        return res.status(500).send("something went wrong")
+    }
+}
+// const bookDemo = async(req, res) =>{
+
+//   const { name, email, phone, organisation, known } = req.body;
+
+//   const userFound = await UserModel.findOne({ email: email });
+  
+//   try {
+//     if (!userFound) {
+//     return res.status(404).send({message: "User is not registered, signup first"});
+//   }
+//     if(!name || !email || !phone || !organisation || !known){
+//       return res.status(401).send("Please enter all the deatils");
+//     }
+//     const demoUser = await new BookModel({
+//       name,
+//       email,
+//       phone,
+//       organisation,
+//       known
+//     }).save(); 
+//              res.status(200).send({
+//            success: true,
+//             message: "User Demobooking successful",
+//     });
+//   } catch (error) {
+//     console.log(error.message);
+//     res.status(500).send({message: "Something went wrong"});
+//   }
+//   res.send({message: "Protected route"});
+// }
+const userdetails= async (req,res)=>{
+  const { token } = req.body;
+  console.log(token)
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET, (err,res)=>{
+            if(err){
+                return "token expired";
+            }
+            return res;
+        });
+        if(user=="token expires"){
+            return res.json({status: "error", data:"token expired"})
+        }
+        const useremail = user.email;
+        UserModel.findOne({email: useremail})
+        .then((data)=>{
+            res.send({status: "ok", data: data});
+        })
+        .catch((error)=>{
+            res.send({status: "error", data: data});
+        })
+    } catch (error) {
+        
+    }
 }
 
 module.exports = {
   signup,
   login,
-  bookDemo
+  bookDemo,
+  userdetails
 };
--- Route/Router.js
+++ Route/Router.js
@@ -6,6 +6,7 @@ const middleFunc = require('../Middleware/userMiddleware');
 route.post('/signup', userFunc.signup);
 route.post('/login',userFunc.login );
 route.post('/bookDemo', middleFunc.requiresSignIn, userFunc.bookDemo);
+route.post('/userdetails',userFunc.userdetails)
 
 
 module.exports = route;
